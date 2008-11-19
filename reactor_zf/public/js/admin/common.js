cb786795b5110c7de3161b362b4a51dff4487865
Marcin Lulek <info@webreactor.eu>
2008-11-19 12:56:21 +0100

    config fix for port setup

--- reactor_zf/public/js/admin/common.js
+++ reactor_zf/public/js/admin/common.js
@@ -1,5 +1,13 @@
 init = function (){
     dojo.query('#loadingJSResources').orphan();
+    /*building top menu
+    
+	cp1 = new dijit.layout.ContentPane({title: ''});
+	cp1.domNode.innerHTML = "Contents of Tab 1";
+	
+	tc = new dijit.layout.TabContainer({style:'height:40px;'}, dojo.byId('topMenu'));
+	tc.addChild(cp1);
+	*/
 }
 
 dojo.addOnLoad(init);
