7ba7f7390e2b04f32f321a61abe17e4107512153
Marcin Lulek <info@webreactor.eu>
2008-11-20 15:56:56 +0100

    temporary do not use

--- reactor_zf/public/js/admin/common.js
+++ reactor_zf/public/js/admin/common.js
@@ -4,15 +4,16 @@ init = function (){
     dojo.query('#topMenu .section').style('display','none');
     dojo.query('#topMenu .types li').forEach(function(elem){
     	dojo.connect(elem,'onmouseover',function(){
-    		dojo.animateProperty({node:elem, duration:200, properties: {borderBottomColor:'#ffcc00',}}).play();
-    		dojo.query('#topMenu .section').style('display','none');
+    		dojo.animateProperty({node:elem, duration:300, properties: {borderBottomColor:'#ffcc00',}}).play();
     		dojo.query('#topMenu .section.'+elem.className).style('display','block');
-    	});
-    	//TODO make the code more efficient and work only on mouseover
-    	dojo.connect(elem,'onmouseout',function(){
-    		dojo.animateProperty({node:elem, duration:200, properties: {borderBottomColor:'353535',},onEnd:function(){
-    			elem.style.borderBottomColor='transparent'}}
-    		).play();
+    		dojo.query('#topMenu .section:not([class~="'+ elem.className +'"])').style('display','none');
+    		dojo.query('#topMenu .types li:not([class~="'+ elem.className +'"])').forEach(function(elem2){
+    			if(elem2.style.borderBottomColor!='transparent'){
+    			dojo.animateProperty({node:elem2, duration:300, properties: {borderBottomColor:'353535',},onEnd:function(){
+        			elem2.style.borderBottomColor='transparent'}}
+        		).play();
+    			}
+    		});
     	});
     });
 }
