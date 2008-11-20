8aa7377ef2aa90cb98d7cca9ac08b050de12f29e
Marcin Lulek <info@webreactor.eu>
2008-11-20 17:51:53 +0100

    better menu

--- reactor_zf/public/js/admin/common.js
+++ reactor_zf/public/js/admin/common.js
@@ -1,7 +1,7 @@
 init = function (){
     dojo.query('#loadingJSResources').orphan();
-    /*building top menu*/
-    dojo.query('#topMenu .section').style('display','none');
+    /* building top menu */
+    dojo.query('#topMenu ul.section:not([class~="mainManagement"])').style('display','none');
     dojo.query('#topMenu .types li').forEach(function(elem){
     	dojo.connect(elem,'onmouseover',function(){
     		dojo.animateProperty({node:elem, duration:300, properties: {borderBottomColor:'#ffcc00',}}).play();
@@ -9,7 +9,7 @@ init = function (){
     		dojo.query('#topMenu .section:not([class~="'+ elem.className +'"])').style('display','none');
     		dojo.query('#topMenu .types li:not([class~="'+ elem.className +'"])').forEach(function(elem2){
     			if(elem2.style.borderBottomColor!='transparent'){
-    			dojo.animateProperty({node:elem2, duration:300, properties: {borderBottomColor:'353535',},onEnd:function(){
+    			dojo.animateProperty({node:elem2, duration:300, properties: {borderBottomColor:'454545',},onEnd:function(){
         			elem2.style.borderBottomColor='transparent'}}
         		).play();
     			}
