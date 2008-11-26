a185e983b194411dc0d7c51b46b4347267daa9c6
Marcin Lulek <info@webreactor.eu>
2008-11-26 15:26:34 +0100

    top menu fixed

--- reactor_zf/public/js/admin/common.js
+++ reactor_zf/public/js/admin/common.js
@@ -13,13 +13,15 @@ trOut = function (){
 init = function (){
     dojo.query('#loadingJSResources').orphan();
     /* building top menu */
-    dojo.query('#topMenu ul.section:not([class~="mainManagement"])').style('display','none');
-    dojo.query('#topMenu .types li').forEach(function(elem){
+    topMenu = dojo.byId('topMenu');
+    dojo.query('ul.section:not([class~="active"])',topMenu).style('display','none');
+    dojo.query('ul.types li.active',topMenu).style('borderBottomColor','#ffcc00');
+    dojo.query('.types li',topMenu).forEach(function(elem){
     	dojo.connect(elem,'onclick',function(){
     		dojo.animateProperty({node:elem, duration:300, properties: {borderBottomColor:'#ffcc00',}}).play();
-    		dojo.query('#topMenu .section.'+elem.className).style('display','block');
-    		dojo.query('#topMenu .section:not([class~="'+ elem.className +'"])').style('display','none');
-    		dojo.query('#topMenu .types li:not([class~="'+ elem.className +'"])').forEach(function(elem2){
+    		dojo.query('.section[class~="'+elem.className +'"])',topMenu).style('display','block');
+    		dojo.query('.section:not([class~="'+ elem.className +'"])',topMenu).style('display','none');
+    		dojo.query('.types li:not([class~="'+ elem.className +'"])',topMenu).forEach(function(elem2){
     			if(elem2.style.borderBottomColor!='transparent'){
     			dojo.animateProperty({node:elem2, duration:300, properties: {borderBottomColor:'454545',},onEnd:function(){
         			elem2.style.borderBottomColor='transparent'}}
